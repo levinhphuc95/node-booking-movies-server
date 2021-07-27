1188268838b47901d60c9944d3702eeac581d0dd
Lê Vĩnh Phúc <72752302+levinhphuc95@users.noreply.github.com>
2021-07-27 20:57:13 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -7,7 +7,6 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   dialect: dbConfig.dialect,
   operatorsAliases: false,
 
-
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
